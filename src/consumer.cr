20e8d0d91362b4383b711981c9ad9339516b41a8
lukas8219 <lucas.c4d@gmail.com>
2025-07-14 23:40:06 -0300

    wip

--- src/consumer.cr
+++ src/consumer.cr
@@ -8,22 +8,24 @@ require "./payment_types"
 class Consumer
   @circuit_breaker : CircuitBreakerWrapper
   @processed_payments : Mongo::Collection
-  @successful_batches : Array(Hash(String, PaymentProcessorRequest | String))
+  @successful_batches : Array(BSON)
   @last_insert : Time
   @pubsub_client : PubSubClient
+  @array_mutex : Mutex
 
   def initialize
     amqp_url = ENV["AMQP_URL"]? || "amqp://guest:guest@localhost:5672/"
     @pubsub_client = PubSubClient.new(amqp_url)
     process_client = HttpClient.new("default", @pubsub_client, ENV["PROCESSOR_URL"]? || "http://localhost:8001")
     fallback_client = HttpClient.new("fallback", @pubsub_client, ENV["FALLBACK_URL"]? || "http://localhost:8002")
+    @array_mutex = Mutex.new
     
     @circuit_breaker = CircuitBreakerWrapper.new(process_client, fallback_client)
     
     mongo_client = MongoClient.instance
     @processed_payments = mongo_client.db("challenge").collection("processed_payments")
     
-    @successful_batches = [] of Hash(String, PaymentProcessorRequest | String)
+    @successful_batches = [] of BSON
     @last_insert = Time.utc
     start_batch_timer
   end
@@ -38,29 +40,34 @@ class Consumer
   end
 
   def add_successful_payment(payment : PaymentProcessorRequest, processor : String)
-    @successful_batches << {
-      "payment" => payment,
-      "processor" => processor
-    }
+    bson_doc = to_bson(payment, processor)
+    if ENV["SKIP_BATCH"]?
+      @processed_payments.insert_one(bson_doc)
+    else
+      @successful_batches << bson_doc
+    end
+  end
+
+  def to_bson(payment : PaymentProcessorRequest, processor : String)
+    BSON.new({
+      "correlationId" => payment.correlationId,
+      "amount" => payment.amount.to_f,
+      "processor" => processor,
+      "timestamp" => payment.requestedAt
+    })
   end
 
   def trigger_process
     return if @successful_batches.empty?
+    @array_mutex.lock
 
     puts "Starting Insert Batch"
     to_insert = @successful_batches.dup
     @successful_batches.clear
     @last_insert = Time.utc
+    @array_mutex.unlock
 
-    bson_docs = to_insert.map do |entry|
-      BSON.new({
-        "correlationId" => entry["payment"].as(PaymentProcessorRequest).correlationId,
-        "amount" => entry["payment"].as(PaymentProcessorRequest).amount.to_f,
-        "processor" => entry["processor"].to_s,
-        "timestamp" => entry["payment"].as(PaymentProcessorRequest).requestedAt
-      })
-    end
-    @processed_payments.insert_many(bson_docs)
+    @processed_payments.insert_many(to_insert)
   end
 
   def run
