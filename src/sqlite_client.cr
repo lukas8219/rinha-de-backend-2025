0ecadfc8433173526179c38418bc6364ab7a685b
Lucas Weis Polesello <75316334+lukas8219@users.noreply.github.com>
2025-07-20 17:30:58 -0300

    perf: changed to unix sockets, less alloc, move float logic inside sqlite and chance cache_size (#14)

--- src/sqlite_client.cr
+++ src/sqlite_client.cr
@@ -8,12 +8,25 @@ class SqliteClient
   end
 
   @db : DB::Database
+  @summary_statement : DB::PoolStatement
 
   def initialize
     db_path = ENV["SQLITE_PATH"]? || "database.sqlite3"
     @db = DB.open("sqlite3://#{db_path}")
     @db.exec("PRAGMA journal_mode=WAL;")
+    @db.exec("PRAGMA cache_size=-5120;")
     @db.exec("CREATE TABLE IF NOT EXISTS processed_payments (id CHAR(36) PRIMARY KEY, timestamp TIMESTAMP, amount INT, processor CHAR(8));")
+    sql = <<-SQL
+      SELECT processor, COUNT(id) AS totalRequests, COALESCE(SUM(amount), 0) / 100.0 AS totalAmount
+      FROM processed_payments
+      WHERE timestamp >= ? AND timestamp <= ?
+      GROUP BY processor
+    SQL
+    @summary_statement = @db.build(sql)
+  end
+
+  def query_summary(from_time : String, to_time : String, &block : DB::ResultSet ->)
+    @summary_statement.query(from_time, to_time, &block)
   end
 
   def db
