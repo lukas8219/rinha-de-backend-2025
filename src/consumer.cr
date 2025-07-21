bdd27af0a4265d3b6683aad48223b486a29120a6
lukas8219 <lucas.c4d@gmail.com>
2025-07-21 19:45:06 -0300

    Remove unnecessary lock now that we have proper CPU reservation and shares

--- src/consumer.cr
+++ src/consumer.cr
@@ -14,8 +14,6 @@ class Consumer
   @pubsub_client : PubSubClient
   property default_skiplist : Skiplist
   property fallback_skiplist : Skiplist
-  @default_mutex = Mutex.new
-  @fallback_mutex = Mutex.new
 
   def initialize
     amqp_url = ENV["AMQP_URL"]? || "amqp://guest:guest@localhost:5672/"
@@ -30,13 +28,9 @@ class Consumer
 
   def add_successful_payment(payment : PaymentProcessorRequest, processor : String)
     if processor == "default"
-      @default_mutex.synchronize do
-        @default_skiplist.insert(Time.parse_iso8601(payment.requestedAt.not_nil!).to_unix_ms.to_f, (payment.amount * 100.0).round.to_i64)
-      end
+      @default_skiplist.insert(Time.parse_iso8601(payment.requestedAt.not_nil!).to_unix_ms.to_f, (payment.amount * 100.0).round.to_i64)
     else
-      @fallback_mutex.synchronize do
-        @fallback_skiplist.insert(Time.parse_iso8601(payment.requestedAt.not_nil!).to_unix_ms.to_f, (payment.amount * 100.0).round.to_i64)
-      end
+      @fallback_skiplist.insert(Time.parse_iso8601(payment.requestedAt.not_nil!).to_unix_ms.to_f, (payment.amount * 100.0).round.to_i64)
     end
   end
 
