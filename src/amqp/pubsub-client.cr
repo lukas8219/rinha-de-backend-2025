7f0023eab6e93e32e690ef91bbb4cd48ee6eb71b
lukas8219 <lucas.c4d@gmail.com>
2025-07-27 23:24:33 -0300

    final adjustment

--- src/amqp/pubsub-client.cr
+++ src/amqp/pubsub-client.cr
@@ -7,7 +7,7 @@ class PubSubClient
   @worker_exchange : AMQP::Client::Exchange
   @current_shard_index : Atomic(Int32)
   def initialize(url : String)
-    @connection = AMQP::Client.new(url, 5672).connect
+    @connection = AMQP::Client.new(url).connect
     @channel = @connection.channel
     @queue_name = "processor:queue:#{ENV["HOSTNAME"]? || "localhost"}"
     @health_exchange = @channel.fanout_exchange()
